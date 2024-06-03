5fd2b05c51e0a3cc6abf04ca2ba5c7914d8e3adb
Yangyu Chen <cyy@cyyself.name>
2024-06-03 16:04:19 +0800

    ifunc_example: provide zvbb, v implementation

--- ifunc_example/ror32.c
+++ ifunc_example/ror32.c
@@ -2,6 +2,20 @@
 #include "ror32.h"
 #include <limits.h>
 
+__attribute__((target("arch=+zbb,+zvbb"))) size_t ror32_zbb_zvbb(unsigned int *a, unsigned int b, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        a[i] = a[i] >> b | (a[i] << (32 - b));
+    }
+    return size;
+}
+
+__attribute__((target("arch=+zbb,+v"))) size_t ror32_zbb_v(unsigned int *a, unsigned int b, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        a[i] = a[i] >> b | (a[i] << (32 - b));
+    }
+    return size;
+}
+
 __attribute__((target("arch=+zbb"))) size_t ror32_zbb(unsigned int *a, unsigned int b, size_t size) {
     for (size_t i = 0; i < size; i++) {
         a[i] = a[i] >> b | (a[i] << (32 - b));
@@ -17,7 +31,9 @@ size_t ror32_normal(unsigned int *a, unsigned int b, size_t size) {
 }
 
 static size_t (*resolve_ror32(unsigned int a, unsigned int b, size_t size))() {
-    if (riscv_has_zbb()) return ror32_zbb;
+    if (riscv_has_zvbb() && riscv_has_zbb()) return ror32_zbb_zvbb;
+    else if (riscv_has_zbb() && riscv_has_v()) return ror32_zbb_v;
+    else if (riscv_has_zbb()) return ror32_zbb;
     else return ror32_normal;
 }
 
