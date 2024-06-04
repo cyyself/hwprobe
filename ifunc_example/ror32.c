3c326bafd18aa8f9e08d47e1033d8b6abf4bb5dc
Yangyu Chen <cyy@cyyself.name>
2024-06-05 00:52:22 +0800

    ifunc_example: insert newline after target declaration

--- ifunc_example/ror32.c
+++ ifunc_example/ror32.c
@@ -2,21 +2,24 @@
 #include "ror32.h"
 #include <limits.h>
 
-__attribute__((target("arch=+zbb,+v,+zvbb"))) size_t ror32_zbb_zvbb(unsigned int *a, unsigned int b, size_t size) {
+__attribute__((target("arch=+zbb,+v,+zvbb")))
+size_t ror32_zbb_zvbb(unsigned int *a, unsigned int b, size_t size) {
     for (size_t i = 0; i < size; i++) {
         a[i] = a[i] >> b | (a[i] << (32 - b));
     }
     return size;
 }
 
-__attribute__((target("arch=+zbb,+v"))) size_t ror32_zbb_v(unsigned int *a, unsigned int b, size_t size) {
+__attribute__((target("arch=+zbb,+v")))
+size_t ror32_zbb_v(unsigned int *a, unsigned int b, size_t size) {
     for (size_t i = 0; i < size; i++) {
         a[i] = a[i] >> b | (a[i] << (32 - b));
     }
     return size;
 }
 
-__attribute__((target("arch=+zbb"))) size_t ror32_zbb(unsigned int *a, unsigned int b, size_t size) {
+__attribute__((target("arch=+zbb")))
+size_t ror32_zbb(unsigned int *a, unsigned int b, size_t size) {
     for (size_t i = 0; i < size; i++) {
         a[i] = a[i] >> b | (a[i] << (32 - b));
     }
@@ -37,4 +40,5 @@ static size_t (*resolve_ror32(unsigned int a, unsigned int b, size_t size))() {
     else return ror32_normal;
 }
 
-__attribute__ ((ifunc ("resolve_ror32"))) size_t ror32(unsigned int *a, unsigned int b, size_t size);
+__attribute__ ((ifunc ("resolve_ror32")))
+size_t ror32(unsigned int *a, unsigned int b, size_t size);
